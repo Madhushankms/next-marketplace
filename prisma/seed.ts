c5f81abe8d843716e31e8953bc115baec38b8822
Malinda-invos <142488296+Madhushankms@users.noreply.github.com>
2026-03-28 18:50:28 +0530

    add Product and Category models with migrations

---
+++ prisma/seed.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "../app/generated/prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Clear existing data
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
