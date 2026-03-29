b74d4dfa9f0082921765fa25af9143794650d577
Malinda-invos <142488296+Madhushankms@users.noreply.github.com>
2026-03-29 17:57:03 +0530

    feat: enhance product display with pagination and loading skeletons

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -2,28 +2,38 @@
 import { formatPrice } from "@/lib/utils";
 import Image from "next/image";
 import { Product } from "./generated/prisma/client";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function ProductCard({ product }: { product: Product }) {
   return (
-    <div className="border border-gray-200 rounded-lg p-4">
+    <Card className="pt-0 overflow-hidden min-h-100">
       <div className="relative aspect-video">
         {product.image && (
           <Image
             src={product.image}
             alt={product.name}
             className="object-cover"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         )}
       </div>
 
-      <h2 className="text-xl font-semibold mt-4">{product.name}</h2>
-      <div className="text-xl font-bold text-green-500">
-        {formatPrice(product.price)}
-      </div>
-      <p className="text-gray-600 mb-4">{product.description}</p>
-    </div>
+      <CardHeader>
+        <CardTitle>{product.name}</CardTitle>
+        <CardDescription>{product.description}</CardDescription>
+      </CardHeader>
+
+      <CardFooter>
+        <p>{formatPrice(product.price)}</p>
+      </CardFooter>
+    </Card>
   );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,78 @@
 import ProductCard from "./ProductCard";
 import { prisma } from "@/lib/prisma";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { Suspense } from "react";
+import ProductSkeleton from "./ProductSkeleton";
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+const pageSize = 3;
 
-export default async function Home() {
-  const products = await prisma.product.findMany();
+async function Products({ page }: { page: number }) {
+  const skip = (page - 1) * pageSize;
 
+  const products = await prisma.product.findMany({
+    skip,
+    take: pageSize,
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Home</h1>
+    <>
       <p>Showing {products.length} products</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+    </>
+  );
+}
+
+export default async function HomePage(props: { searchParams: SearchParams }) {
+  const searchParams = await props.searchParams;
+  const page = Number(searchParams.page) || 1;
+
+  const total = await prisma.product.count();
+
+  const totalPages = Math.ceil(total / pageSize);
+
+  return (
+    <main className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Home</h1>
+
+      <Suspense key={page} fallback={<ProductSkeleton />}>
+        <Products page={page} />
+      </Suspense>
+
+      <Pagination className="mt-8">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href={`?page=${page - 1}`} />
+          </PaginationItem>
+
+          {Array.from({ length: totalPages }, (_, index) => (
+            <PaginationItem key={index}>
+              <PaginationLink
+                href={`?page=${index + 1}`}
+                isActive={page === index + 1}
+              >
+                {index + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+
+          <PaginationItem>
+            <PaginationNext href={`?page=${page + 1}`} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </main>
   );
 }
